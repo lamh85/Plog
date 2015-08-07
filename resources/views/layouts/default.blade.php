15e8374d5950c0b4dd7150338a151c56152787bc
Hon Lam <lamh85@gmail.com>
2015-08-06 22:51:09 -0700

    Improved layout view.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,13 +2,22 @@
 
 <head>
 
+  <!-- JQUERY -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <!-- BOOTSTRAP CSS -->
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <!-- BOOTSTRAP JS -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  
 </head>
 
 <body>
 
-  Menu:
+  Menu: 
+  <a href="/">Home</a> |
+  <a href="/about">About Me</a>
   <hr>
-
+  <br>
   @yield('content')
 </body>
 
