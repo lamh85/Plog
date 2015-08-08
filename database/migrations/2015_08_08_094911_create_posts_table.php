b4755de1c18e927cf9e3395a5a7a5337a4b6ce24
Hon Lam <lamh85@gmail.com>
2015-08-08 02:54:04 -0700

    Created migration to create Posts table.

---
+++ database/migrations/2015_08_08_094911_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('posts', function(Blueprint $table) {
+            $table->increments('id');
+            $table->stirng('title');
+            $table->longText('body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
