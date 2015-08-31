ffa63e0fb54a3d0ae11fe983746c58253c602c5c
Hon Lam <lamh85@gmail.com>
2015-08-30 23:25:33 -0700

    Working on PUT.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 
-  <h1>Create a new blog post!</h1>
+<h1>Create a new blog post!</h1>
 
-  <form action="/post" method="POST">
-    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+<form action="/post" method="POST">
+  <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 
-    <label for="title">Title</label>
-    <input name="title">
+  <label for="title">Title</label>
+  <input name="title">
 
-    <label for="body">Body</label>
-    <input name="body">
+  <label for="body">Body</label>
+  <input name="body">
 
-    <button type="submit">Submit!</button>
-  </form>
+  <button type="submit">Submit!</button>
+</form>
 
 @stop
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,4 +7,17 @@
 <br>Title: {{ $post->title }}
 <br>Body: {{ $post->body }}
 
+<form action="/post/{{$post->id}}" method="POST">
+  <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+  <input type="hidden" name="_method" value="PUT">
+
+  <label for="title">Title</label>
+  <input name="title" value="{{$post->title}}">
+
+  <label for="body">Body</label>
+  <input name="body" value="{{$post->body"}}>
+
+  <button type="submit">Submit!</button>
+</form>
+
 @stop
