527b30f78d5d8277ed5c90d62cd7287adaa05a39
Hon Lam <lamh85@gmail.com>
2015-08-31 00:45:49 -0700

    Edited form in the SHOW view.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,11 +11,15 @@
   <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
   <input type="hidden" name="_method" value="PUT">
 
+  <p>
   <label for="title">Title</label>
+  <br>
   <input name="title" value="{{$post->title}}">
 
+  <p>
   <label for="body">Body</label>
-  <input name="body" value="{{$post->body}}">
+  <br>
+  <textarea name="body">{{$post->body}}</textarea>
 
   <button type="submit">Submit!</button>
 </form>
