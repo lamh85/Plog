c80a08a3dc2094209f41118a99d856778bd0f273
Hon Lam <lamh85@gmail.com>
2015-09-23 20:56:07 -0700

    Navbar changes depending on whether authenticated.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <!-- BOOTSTRAP JS -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  
+
   <title>
     @yield('title')
   </title>
@@ -23,10 +23,12 @@
   <a href="/about">About Me</a> | 
   <a href="{{URL::route('posts.create')}}">New Blog Post</a> |
   <a href="{{URL::route('posts.index')}}">All Blog Posts</a> |
-  <a href="{{URL::route('users.create')}}">Register</a> |
-  <a href="auth/login">Login</a>
   @if (Auth::check())
-    <br>You currently signed in!
+    You currently signed in! -
+    <a href="auth/logout" onclick="return confirm('Do you really wish to log out?')">Log Out</a>
+  @else
+    <a href="{{URL::route('users.create')}}">Register</a> |
+    <a href="auth/login">Log In</a>
   @endIf
   <hr>
   <br>
